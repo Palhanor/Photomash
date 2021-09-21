e4a562df4aaebdb92ee85ef34aaf4b999db459ec
Lucas Palhano Fonseca <lucas_palhano_@hotmail.com>
2021-09-20 23:00:58 -0300

    Scss and class adjusts

--- js/ranking.js
+++ js/ranking.js
@@ -1,23 +1,24 @@
-var photomashScreen = document.querySelector("#photomash");
-var photomash = document.querySelector("#logoName");
-var ranking = document.querySelector("#ranking");
-var rankingScreen = document.querySelector("#ranking-screen");
+var photomash = document.querySelector(".header__logo");
+var ranking = document.querySelector(".header__ranking");
+var photomashScreen = document.querySelector(".photomash-screen");
+var rankingScreen = document.querySelector(".ranking-screen");
 
 function openRanking() {
     photomashScreen.style.display = "none";
     rankingScreen.style.display = "block";
+    rankingScreen.innerHTML = "";
 
     for (let i = 0; i < numFotos; i++) {
         var rankingItem = document.createElement("div");
-        rankingItem.classList.add("ranking-box");
+        rankingItem.classList.add("ranking-screen__box");
         var rankingImage = document.createElement("img");
-        rankingImage.classList.add("ranking-image");
+        rankingImage.classList.add("ranking-screen__image");
         var scrPath = bancoDeFotos[i];
         scrPath = scrPath.replace("url('", "");
         scrPath = scrPath.replace("')", "");
         rankingImage.src = scrPath;
         var rankingScore = document.createElement("span");
-        rankingScore.classList.add("ranking-text");
+        rankingScore.classList.add("ranking-screen__text");
         rankingScore.textContent = placar[i];
 
         rankingScreen.appendChild(rankingItem);
