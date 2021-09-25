b0e30d7c0efa34719497d03209d0a85a468ba665
Lucas Palhano Fonseca <lucas_palhano_@hotmail.com>
2021-09-25 18:57:38 -0300

    adjusting style and sorting the ranking

--- js/ranking.js
+++ js/ranking.js
@@ -1,27 +1,36 @@
 var photomash = document.querySelector(".header__logo");
-var ranking = document.querySelector(".header__ranking");
+var ranking = document.querySelector(".ranking");
 var photomashScreen = document.querySelector(".photomash-screen");
 var rankingScreen = document.querySelector(".ranking-screen");
 var body = document.querySelector("body");
+var scoreRanking = [];
 
 function openRanking() {
+    // Create an copy of the database
+    scoreRanking = [];
+    for (let i = 0; i < bancoDeFotos.length; i++) {
+        scoreRanking.push(bancoDeFotos[i]);
+    }
+    // Sort the new database by score
+    scoreRanking.sort((a, b) => b.score - a.score);
+
     photomashScreen.style.display = "none";
     rankingScreen.style.display = "block";
     rankingScreen.innerHTML = "";
     body.style.overflow = "scroll";
 
-    for (let i = 0; i < bancoDeFotos.length; i++) {
+    for (let i = 0; i < scoreRanking.length; i++) {
         var rankingItem = document.createElement("div");
         rankingItem.classList.add("ranking-screen__box");
         var rankingImage = document.createElement("img");
         rankingImage.classList.add("ranking-screen__image");
-        var scrPath = bancoDeFotos[i].img;
+        var scrPath = scoreRanking[i].img;
         scrPath = scrPath.replace("url('", "");
         scrPath = scrPath.replace("')", "");
         rankingImage.src = scrPath;
         var rankingScore = document.createElement("span");
         rankingScore.classList.add("ranking-screen__text");
-        rankingScore.textContent = bancoDeFotos[i].score;
+        rankingScore.textContent = scoreRanking[i].score;
 
         rankingScreen.appendChild(rankingItem);
         rankingItem.appendChild(rankingImage);
@@ -34,6 +43,7 @@ function closeRanking() {
     rankingScreen.style.display = "none";
     rankingScreen.innerHTML = "";
     body.style.overflow = "hidden";
+    scoreRanking = [];
 }
 
 photomash.addEventListener("click", closeRanking);
